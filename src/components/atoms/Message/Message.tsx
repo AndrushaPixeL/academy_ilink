b0b4681a0a06474ce53003560260985a8ce335f9
Andrey Surnov <surnov.andrey@mail>
2021-10-30 18:38:18 +0300

    feat: added messages and scrollbar for chat + styleds

--- src/components/atoms/Message/Message.tsx
+++ src/components/atoms/Message/Message.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react'
 import './Message.scss'
 
-const Message: FC = () => {
+interface Message {
+  classNameMessage: string
+}
+
+const Message: FC<Message> = ({ classNameMessage }) => {
   return (
-    <div className="message_container">
-      <div className="message">Message</div>
+    <div className={classNameMessage}>
+      <div className="message_text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        incidunt ipsam ducimus quidem quae minima nemo veritatis distinctio
+        asperiores beatae fugit provident eaque aperiam, voluptas tenetur ullam
+        odio animi odit!
+      </div>
     </div>
   )
 }
--- src/components/organisms/Chat/Chat.tsx
+++ src/components/organisms/Chat/Chat.tsx
@@ -3,27 +3,36 @@ import Message from '../../atoms/Message/Message'
 import NoSelectChatNotice from '../../atoms/NoSelectChatNotice/NoSelectChatNotice'
 import './Chat.scss'
 
-const Chats: FC = () => {
+const Chat: FC = () => {
   const noSelectedChat = false
-
+  const classNameMyMessage = 'chat_myMessage'
+  const classNameYourMessage = 'chat_yourMessage'
   return (
-    <div className="chat">
-      {noSelectedChat ? (
-        <div className="chat_noSelect_container">
-          <NoSelectChatNotice />
-        </div>
-      ) : (
-        <>
-          <div className="chat_myMessage">
-            <Message />
+    <div className="chat_container">
+      <div className="chat">
+        {noSelectedChat ? (
+          <div className="chat_noSelect_container">
+            <NoSelectChatNotice />
           </div>
-          <div className="chat_myMessage">
-            <Message />
-          </div>
-        </>
-      )}
+        ) : (
+          <>
+            <div className={classNameMyMessage}>
+              <Message classNameMessage="myMessage" />
+            </div>
+            <div className={classNameYourMessage}>
+              <Message classNameMessage="yourMessage" />
+            </div>
+            <div className={classNameMyMessage}>
+              <Message classNameMessage="myMessage" />
+            </div>
+            <div className={classNameYourMessage}>
+              <Message classNameMessage="yourMessage" />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
 
-export default Chats
+export default Chat
